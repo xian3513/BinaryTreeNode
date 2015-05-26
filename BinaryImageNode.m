f174c33936f17314f94069160a7d2d8393a74e6b
kt <kt@a2ndeiMac.local>
2015-05-26 15:32:01 +0800

    first commit

---
+++ BinaryImageNode.m
@@ -0,0 +1,13 @@
+//
+//  BinaryImageNode.m
+//  BinaryImage
+//
+//  Created by kt on 15/5/26.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import "BinaryImageNode.h"
+
+@implementation BinaryImageNode
+
+@end
