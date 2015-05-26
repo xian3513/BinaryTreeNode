c301164b26311649e1c0fce06bf79be5d7cfc6c1
kt <kt@a2ndeiMac.local>
2015-05-26 15:19:50 +0800

    Initial Commit

---
+++ BinaryImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BinaryImage
+//
+//  Created by kt on 15/5/26.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
